b3964112680512a5afeb581ddfb9e01bc4991d41
Talkanbaev Artur <talkanbaev_a@auca.kg>
2022-02-01 16:55:42 +0600

    Updated handler to return json erros

--- user/http/handlers.go
+++ user/http/handlers.go
@@ -2,7 +2,6 @@ package http
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"strconv"
 
@@ -11,17 +10,27 @@ import (
 	"go.uber.org/zap"
 )
 
+type errorMsg struct {
+	Message string `json:"msg"`
+	Details string `json:"err_details"`
+}
+
+func answerErr(err errorMsg, rw http.ResponseWriter, logr *zap.SugaredLogger) {
+	rw.WriteHeader(500)
+	logr.Errorw(err.Message, "error", err.Details)
+	json.NewEncoder(rw).Encode(err)
+}
+
 func createHandleListUsers(us service.Service) http.HandlerFunc {
 	logr := zap.S().With("level", "httpServer")
 	return func(rw http.ResponseWriter, r *http.Request) {
 		users, err := us.ListUser(r.Context())
 		if err != nil {
-			logr.Errorw("error happended during fetch process", "error", err.Error())
-			http.Error(rw, fmt.Sprintf("error happended during fetch process, erorr: %s", err.Error()), 500)
+			answerErr(errorMsg{"error happended during fetch process", err.Error()}, rw, logr)
 			return
 		}
 		rw.WriteHeader(200)
-		json.NewEncoder(rw).Encode(&users)
+		json.NewEncoder(rw).Encode(users)
 		logr.Infow("Succesfully returned users list answer")
 	}
 }
@@ -35,8 +44,7 @@ func createHandlerGetUser(us service.Service) http.HandlerFunc {
 
 		users, err := us.GetUserByID(r.Context(), id)
 		if err != nil {
-			logr.Errorw("error happended during fetch process", "error", err.Error())
-			http.Error(rw, fmt.Sprintf("error happended during fetch process, erorr: %s", err.Error()), 500)
+			answerErr(errorMsg{"error happended during fetch process", err.Error()}, rw, logr)
 			return
 		}
 
@@ -52,15 +60,13 @@ func createHandlerCreateUser(us service.Service) http.HandlerFunc {
 		var req service.UserChangeInput
 		err := json.NewDecoder(r.Body).Decode(&req)
 		if err != nil {
-			logr.Errorw("failed to parse request params", "error", err.Error())
-			http.Error(rw, fmt.Sprintf("failed to parse request params, erorr: %s", err.Error()), 500)
+			answerErr(errorMsg{"failed to parse request params", err.Error()}, rw, logr)
 			return
 		}
 
 		user, err := us.RegisterUser(r.Context(), req)
 		if err != nil {
-			logr.Errorw("error happended during fetch process", "error", err.Error())
-			http.Error(rw, fmt.Sprintf("error happended during fetch process, erorr: %s", err.Error()), 500)
+			answerErr(errorMsg{"error happended during registration process", err.Error()}, rw, logr)
 			return
 		}
 
@@ -76,8 +82,7 @@ func createHandlerUpdateUser(us service.Service) http.HandlerFunc {
 		var req service.UserChangeInput
 		err := json.NewDecoder(r.Body).Decode(&req)
 		if err != nil {
-			logr.Errorw("failed to parse request params", "error", err.Error())
-			http.Error(rw, fmt.Sprintf("failed to parse request params, erorr: %s", err.Error()), 500)
+			answerErr(errorMsg{"failed to parse request params", err.Error()}, rw, logr)
 			return
 		}
 
@@ -86,8 +91,7 @@ func createHandlerUpdateUser(us service.Service) http.HandlerFunc {
 
 		err = us.UpdateUserAccount(r.Context(), id, req)
 		if err != nil {
-			logr.Errorw("error happended during fetch process", "error", err.Error())
-			http.Error(rw, fmt.Sprintf("error happended during fetch process, erorr: %s", err.Error()), 500)
+			answerErr(errorMsg{"error happended during update process", err.Error()}, rw, logr)
 			return
 		}
 
@@ -105,8 +109,7 @@ func createHandlerDeleteUser(us service.Service) http.HandlerFunc {
 
 		err := us.SuspendUser(r.Context(), id)
 		if err != nil {
-			logr.Errorw("error happended during fetch process", "error", err.Error())
-			http.Error(rw, fmt.Sprintf("error happended during fetch process, erorr: %s", err.Error()), 500)
+			answerErr(errorMsg{"error happended during delete process", err.Error()}, rw, logr)
 			return
 		}
 
